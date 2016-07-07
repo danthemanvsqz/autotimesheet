beabf6525303e0f3665c97e05fc2f8a2da145808
danthemanvsqz <danthemanvsqz@gmail.com>
2016-07-06 18:32:55 -0700

    Junit

---
+++ test/AutoTimesheetTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by vasquezd on 7/6/2016.
+ */
+public class AutoTimesheetTest {
+    @org.junit.Test
+    public void main() throws Exception {
+        assertFalse(true);
+    }
+
+}
