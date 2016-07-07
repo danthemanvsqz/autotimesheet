d9e626476a0b3b91a5e4fcf577f3d4fdb874fed5
spencec <cjspence434@yahoo.com>
2016-07-06 17:47:29 -0700

    Added Maven pom

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -1,10 +1,18 @@
-
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        // Create a new instance of the Firefox driver
+        // Notice that the remainder of the code relies on the interface,
+        // not the implementation.
+        WebDriver driver = new FirefoxDriver();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+
     }
 
 }
