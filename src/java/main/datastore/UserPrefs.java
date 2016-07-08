1978fb36ab524ecbda4bd1869d002c532bae1e91
spencec <cjspence434@yahoo.com>
2016-07-08 11:48:55 -0700

    Added timesheet keys

--- src/java/main/datastore/UserPrefs.java
+++ src/java/main/datastore/UserPrefs.java
@@ -6,10 +6,49 @@ public class UserPrefs {
 
     private String username;
     private String password;
+    private String startTime;
+    private String endTime;
+    private String startDay;
+
+    private static final String MONDAY = "Mon";
+    private static final String TUESDAY = "Tue";
+    private static final String WEDNESDAY = "Wed";
+    private static final String THURSDAY = "Thu";
+    private static final String FRIDAY = "Fri";
+    private static final String SATURDAY = "Sat";
+    private static final String SUNDAY = "Sun";
 
     public UserPrefs (Map<String, String> userData) {
         this.username = userData.get("username");
         this.password = userData.get("password");
+        this.startDay = userData.get("startDay");
+        this.startTime = userData.get("startTime");
+        this.endTime = userData.get("endTime");
+    }
+
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String firstHalfStart) {
+        this.startTime = firstHalfStart;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String secondHalfEnd) {
+        this.endTime = secondHalfEnd;
+    }
+
+    public String getStartDay() {
+        return startDay;
+    }
+
+    public void setStartDay(String days) {
+        this.startDay = days;
     }
 
     public String getUsername() {
